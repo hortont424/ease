66992b927243016970858b584b93f9b87807a652
Robert Carr <carrr@rpi.edu>
2009-01-23 22:21:55 -0500

    [ease @ carrr@rpi.edu-20090124032155-4yy5uskjwtqm4ko1] Add stage

---
+++ ease-stage.c
@@ -0,0 +1,41 @@
+#include "ease-stage.h"
+#include "ease-actor.h"
+
+#include <glib.h>
+#include <glib-object.h>
+#include <clutter.h>
+
+static void ease_actor_interface_init (EaseStageInterface *iface);
+
+G_DEFINE_TYPE_WITH_CODE (EaseActor, ease_actor, CLUTTER_TYPE_STAGE,
+			 G_IMPLEMENT_INTERFACE (EASE_TYPE_ACTOR,
+						ease_actor_interface));
+
+static gboolean 
+ease_stage_do_message (EaseActor *self, GQuark type, gchar *message)
+{
+    g_print ("TODO!\n");
+    
+    return FALSE;
+}
+
+static gchar * 
+ease_stage_serialize (EaseActor *self)
+{
+    g_print ("TODO!\n");
+    
+    return NULL;
+}
+
+static void
+ease_actor_interface_init (EaseActorInterface *iface)
+{
+    iface->do_message = ease_stage_do_message;
+    iface->serialize = ease_stage_serialize;
+}
+
+static void
+ease_stage_init (EaseStage *self)
+{
+    return;
+}
---
+++ ease-stage.h
@@ -0,0 +1,32 @@
+#ifndef __EASE_STAGE_H__
+#define __EASE_STAGE_H__
+
+#include <glib-object.h>
+#include <clutter.h>
+
+#define EASE_TYPE_BAZ             (ease_stage_get_type ())
+#define EASE_STAGE(obj)             (G_TYPE_CHECK_INSTANCE_CAST ((obj), EASE_TYPE_STAGE, EaseStage))
+#define EASE_IS_STAGE(obj)          (G_TYPE_CHECK_INSTANCE_TYPE ((obj), EASE_TYPE_STAGE))
+#define EASE_STAGE_CLASS(klass)     (G_TYPE_CHECK_CLASS_CAST ((klass), EASE_TYPE_STAGE, EaseStageClass))
+#define EASE_IS_STAGE_CLASS(klass)  (G_TYPE_CHECK_CLASS_TYPE ((klass), EASE_TYPE_STAGE))
+#define EASE_STAGE_GET_CLASS(obj)   (G_TYPE_INSTANCE_GET_CLASS ((obj), EASE_TYPE_STAGE, EaseStageClass))
+
+
+typedef struct _EaseStage        EaseStage;
+typedef struct _EaseStageClass   EaseStageClass;
+
+
+struct _EaseStage
+{
+    GObject parent_instance;
+};
+
+struct _EaseStageClass
+{
+    GObjectClass parent_class;
+};
+
+GType ease_stage_get_type (void);
+
+#endif /* __SEED_STAGE_H__ */			\
+		
