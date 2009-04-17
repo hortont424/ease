603e935cb17dec3fbb1acfa06a84f276c95bf8d1
Tim Horton <hortont424@gmail.com>
2009-04-17 13:01:04 -0400

    stuff

--- Example.ease/slides/6.js
+++ Example.ease/slides/6.js
@@ -31,7 +31,7 @@
 		{
 			"ease_name": "main-text",
 			"type": "Ease.TextActor",
-			"text": "Strategically inserting Seed dependencies:",
+			"text": "• Released 0.5 on Wednesday\n• Strategically inserting Seed dependencies:",
 			"font_name": "Bitstream Vera Sans 24",
 			"y": "_(+ (- (/ (- (send slide height) (+ (send charmander-018 height) 100)) 2) (/ (send self height) 2)) 80)",
 			"x": 100,
@@ -40,7 +40,7 @@
 		{
 			"ease_name": "ephy",
 			"type": "Ease.TextActor",
-			"text": "  • Epiphany",
+			"text": "    • Epiphany",
 			"font_name": "Bitstream Vera Sans 24",
 			"y": "_(+ (send main-text height) (send main-text y))",
 			"x": 100,
@@ -49,7 +49,7 @@
 		{
 			"ease_name": "ggames",
 			"type": "Ease.TextActor",
-			"text": "  • GNOME Games",
+			"text": "    • GNOME Games",
 			"font_name": "Bitstream Vera Sans 24",
 			"y": "_(+ (send ephy height) (send ephy y))",
 			"x": 100,
@@ -58,7 +58,7 @@
 		{
 			"ease_name": "nautilus",
 			"type": "Ease.TextActor",
-			"text": "  • Nautilus",
+			"text": "    • Nautilus",
 			"font_name": "Bitstream Vera Sans 24",
 			"y": "_(+ (send ggames height) (send ggames y))",
 			"x": 100,
@@ -67,7 +67,7 @@
 		{
 			"ease_name": "gnomeshell",
 			"type": "Ease.TextActor",
-			"text": "  • GNOME Shell?",
+			"text": "    • GNOME Shell?",
 			"font_name": "Bitstream Vera Sans 24",
 			"y": "_(+ (send nautilus height) (send nautilus y))",
 			"x": 100,
@@ -75,7 +75,8 @@
 		}],
 	"actions": [
 	{
-		"wait": "click",
+		"wait": "time",
+		"duration": 500,
 		"effects":[
 			{
 				"type": "EaseAnimations.Slide",
@@ -88,7 +89,7 @@
 	},
 	{
 		"wait": "time",
-		"duration": 500,
+		"duration": 1000,
 		"effects":[
 			{
 				"type": "EaseAnimations.Slide",
@@ -101,7 +102,7 @@
 	},
 	{
 		"wait": "time",
-		"duration": 1000,
+		"duration": 1500,
 		"effects":[
 			{
 				"type": "EaseAnimations.Slide",
@@ -114,7 +115,7 @@
 	},
 	{
 		"wait": "time",
-		"duration": 1500,
+		"duration": 2000,
 		"effects":[
 			{
 				"type": "EaseAnimations.Slide",
---
+++ Example.ease/slides/7.js
@@ -0,0 +1,42 @@
+{
+	"name": "4",
+	"actors": [
+		{
+			"ease_name": "background",
+			"type": "Ease.ImageActor",
+			"filename": "blobs/bkg3.png",
+			"x": 0,
+			"y": 0,
+			"width": "_(send slide width)",
+			"height": "_(send slide height)",
+		},
+		{
+			"ease_name": "main-text",
+			"type": "Ease.TextActor",
+			"text": "http://hortont.com/ease/wiki\n\nhttp://live.gnome.org/Seed",
+			"font_name": "Bitstream Vera Sans 40",
+			"y": "_(+ (- (/ (- (send slide height) (+ (send charmander-018 height) 100)) 2) (/ (send self height) 2)) 80)",
+			"x": 100,
+			"color": {"red":255, "green":255, "blue":255, "alpha":255}
+		}],
+	"actions": [
+	{
+		"wait": "time",
+		"duration": 500,
+		"effects":[
+			{
+				"type": "EaseAnimations.Scale",
+				"actor": "main-text",
+				"direction": "in",
+				"alpha": "Clutter.AnimationMode.EASE_OUT_SINE",
+				"duration": 1000
+			}]
+	}],
+	"transition": {
+		"type": "EaseTransitions.Scale",
+		"direction": "out",
+		"duration": 1000,
+		"alpha": "Clutter.AnimationMode.EASE_IN_SINE"
+	}
+}
+
