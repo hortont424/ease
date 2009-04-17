7031d52f87693d1d0d20e555a0a6775e64b1fc15
Tim Horton <hortont424@gmail.com>
2009-04-17 03:29:29 -0400

    asdf

---
+++ Example.ease/slides/4.js
@@ -0,0 +1,47 @@
+{
+	"name": "4",
+	"actors": [
+		{
+			"ease_name": "background",
+			"type": "Ease.ImageActor",
+			"filename": "blobs/background.jpg",
+			"x": 0,
+			"y": 0,
+			"width": "_(send slide width)",
+			"height": "_(send slide height)",
+		},
+		{
+			"ease_name": "charmander-019",
+			"type": "Ease.TextActor",
+			"text": "What's Not Done?",
+			"font_name": "Bitstream Vera Sans Bold 60",
+			"x": "_(+ (- (/ (send slide width) 2) (/ (send self width) 2)) 2)",
+			"y": 102,
+			"color": {"red":0, "green":0, "blue":0, "alpha":128}
+		},
+		{
+			"ease_name": "charmander-018",
+			"type": "Ease.TextActor",
+			"text": "What's Not Done?",
+			"font_name": "Bitstream Vera Sans Bold 60",
+			"x": "_(- (/ (send slide width) 2) (/ (send self width) 2))",
+			"y": 100,
+			"color": {"red":255, "green":255, "blue":255, "alpha":255}
+		},
+		{
+			"ease_name": "main-text",
+			"type": "Ease.TextActor",
+			"text": "• Editor\n    • Vital to adoption\n    • Requires much more time",
+			"font_name": "Bitstream Vera Sans 24",
+			"y": "_(+ (- (/ (- (send slide height) (+ (send charmander-018 height) 100)) 2) (/ (send self height) 2)) 160)",
+			"x": 100,
+			"color": {"red":255, "green":255, "blue":255, "alpha":255}
+		}],
+	"transition": {
+		"type": "EaseTransitions.Scale",
+		"direction": "in",
+		"duration": 1000,
+		"alpha": "Clutter.AnimationMode.EASE_IN_SINE"
+	}
+}
+
